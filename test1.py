2cfa9cdb54e4837ab66bd29020492eb2154b3623
patilanirudh <patilanirudha204@gmail.com>
2024-02-16 12:45:50 +0530

    hey

--- test1.py
+++ test1.py
@@ -1,22 +1,2 @@
 print('in main branch')
-a=10
-print("hey")
-if a%2==0:
-    print('even')
-
-
-
-print("understanding conflicts")
-
-print("hi")
-
-
-
-print("klkl")
-# incoming
-# current
-# both
-# compare
-
-print('wakandnd')
-print('ajja')
+print('in test branch')
